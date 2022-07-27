4bb93b14637d44bbec86967f5371037400387c3b
LuichidevUOC <laranamo@uoc.edu>
2022-07-27 18:23:28 +0200

    feat: Add gitignore

--- service/getGifs.js
+++ service/getGifs.js
@@ -1,7 +1,7 @@
-const apikey = '2yYQVQqKyweOHT486tY0aoRJkFKwjjx7'
+import { API_KEY } from '../config.js'
 
 export default function getGifs({ keyword = 'pokemon' } = {}) {
-	const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+	const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
 	return fetch(apiURL)
 		.then((res) => res.json())
 		.then((response) => {
